98120f2a3c1472fcec62a0a4331d6801f1476619
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-22 12:04:05 +0300

    Completed api

--- client/src/api/expenses.ts
+++ client/src/api/expenses.ts
@@ -1,11 +1,32 @@
 import axios from 'axios';
 
-import { IExpense } from '../types/IExpense';
+import { IExpense } from '../types/Expense.interface';
+
+const BaseURL = 'http://localhost:5000/expenses'
 
 export const getExpenses = async () => {
-  const data: IExpense[] = await axios.get(
-    'http://localhost:5000/expenses'
-  ).then(response => response.data);
+  const data: IExpense[] = await axios.get(BaseURL)
+    .then(response => response.data);
 
   return data;
 };
+
+export const getExpenseById = async (expenseId: number) => {
+  const data: IExpense = await axios.get(`${BaseURL}/${expenseId}`)
+    .then(response => response.data);
+
+  return data;
+};
+
+export const removeExpense = async (expenseId: number) => {
+  await axios.delete(`${BaseURL}/${expenseId}`);
+};
+
+export const addExpense = async (expense: Omit<IExpense, 'id'>) => {
+  await axios.post(BaseURL, expense);
+};
+
+export const patchExpense = async (expense: IExpense) => {
+  await axios.patch(`${BaseURL}/${expense.id}`, expense);
+};
+
