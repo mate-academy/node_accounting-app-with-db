f14e7222d3cd79ac957e473719ff576f246b4846
Vadym Dubniak <dubnyakv@gmail.com>
2023-06-05 22:40:36 +0300

    added database client

---
+++ src/utils/db.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres', 'postgres', 'test123', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+module.exports = {
+  sequelize,
+};
