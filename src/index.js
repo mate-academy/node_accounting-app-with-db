f65f28eb8b5429230070fda3cdf5e3eb62e84c8d
Kateryna Sokur <k.o.sokur@gmail.com>
2023-07-27 19:33:14 +0200

    add solution for user and expense db

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@
 const { createServer } = require('./createServer');
 
 createServer().listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Server is running on http://localhost:3000');
 });
