e529b22b075356d4af8ed19c87b2f5e31e2a1f6f
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-10-18 11:06:06 +0300

    replace locale storage with db postgres

---
+++ src/controllers/expenses.controller.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const service = require('../services/expenses.services');
+const { getById: getUserById } = require('../services/users.services');
+
+const getAllExpenses = async(req, res) => {
+  const {
+    userId,
+    from,
+    to,
+    categories,
+  } = req.query;
+
+  let expenses = await service.getAll();
+
+  if (userId) {
+    expenses = expenses.filter(expense => expense.userId === +userId);
+  }
+
+  if (from) {
+    expenses = expenses.filter(expense => {
+      return new Date(expense.spentAt).valueOf() >= new Date(from).valueOf();
+    });
+  }
+
+  if (to) {
+    expenses = expenses.filter(expense => {
+      return new Date(expense.spentAt).valueOf() <= new Date(to).valueOf();
+    });
+  }
+
+  if (categories) {
+    expenses = expenses.filter(
+      expense => categories.includes(expense.category)
+    );
+  }
+
+  res.statusCode = 200;
+  res.send(expenses);
+};
+
+const getExpense = async(req, res) => {
+  const id = +req.params.id;
+
+  if (!id) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const searcedExpense = await service.getById(id);
+
+  if (!searcedExpense) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.statusCode = 200;
+  res.send(searcedExpense);
+};
+
+const post = (req, res) => {
+  const {
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  } = req.body;
+
+  if (!getUserById(+userId)) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const newExpense = {
+    id: +new Date(),
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  };
+
+  service.add(newExpense);
+  res.statusCode = 201;
+  res.send(newExpense);
+};
+
+const removeExpense = (req, res) => {
+  const id = +req.params.id;
+  const expenseToRemove = service.getById(id);
+
+  if (!expenseToRemove) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  service.remove(id);
+  res.sendStatus(204);
+};
+
+const updateExpense = (req, res) => {
+  const id = +req.params.id;
+  const body = req.body;
+
+  if (!id || !body) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  if (!service.getById(id)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const updatedExpense = service.update(id, body);
+
+  res.send(updatedExpense);
+};
+
+module.exports = {
+  getAllExpenses,
+  getExpense,
+  post,
+  removeExpense,
+  updateExpense,
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const { createServer } = require('./createServer');
+
+createServer().listen(3000, () => {
+  /* eslint-disable-next-line */
+  console.log('Server runs on 3000 port');
+});
