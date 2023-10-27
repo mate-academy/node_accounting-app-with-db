a0d52b0fa2e87175d1bf1672fc673eaabd6afd3d
Andrzej Pietkiewicz <pietan@me.com>
2023-10-27 08:59:48 +0200

    errors fixed

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const { createServer } = require('./src/createServer.js');
 
 const PORT = process.env.PORT || 8080;
 
-createServer().listen(3000, () => {
+createServer().listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on localhost:${PORT}`);
 });
--- src/services/database.service.js
+++ src/services/database.service.js
@@ -5,7 +5,12 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.EXT_URL, {
+const url = process.env.EXT_URL
+|| `postgres://accounting_app_spn5_user
+:HxXlZXc6Yg3epYwLvsQ9CItYyX2ycPKv@dpg-cksfat90at9c73d5tem0-a
+.frankfurt-postgres.render.com/accounting_app_spn5`;
+
+const sequelize = new Sequelize(url, {
   dialectOptions: {
     ssl: true,
   },
