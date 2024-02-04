e812aa29fa7df454a68997c522397997759a4da6
Katsiaryna Prakofyeva <catherine.smiff@gmail.com>
2024-02-04 19:37:50 +0100

    fix: added extensions for model files

--- src/db.js
+++ src/db.js
@@ -8,6 +8,34 @@ const utils = require('util');
 require('dotenv').config();
 global.TextEncoder = utils.TextEncoder;
 
+// const {
+//   POSTGRES_HOST,
+//   POSTGRES_PORT,
+//   POSTGRES_USER,
+//   POSTGRES_PASSWORD,
+//   POSTGRES_DB,
+// } = process.env;
+
+/*
+  All credentials setted to default values (exsept password - it is exapmle)
+  replace if needed with your own
+*/
+
+// const sequelize = new Sequelize({
+//   database: POSTGRES_DB || 'postgres',
+//   username: POSTGRES_USER || 'postgres',
+//   host: POSTGRES_HOST || 'localhost',
+//   dialect: 'postgres',
+//   port: POSTGRES_PORT || 5432,
+//   password: POSTGRES_PASSWORD || '123',
+// });
+
+// module.exports = {
+//   sequelize,
+// };
+
+// const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize('postgres', 'postgres', '1280', {
   host: 'localhost',
   dialect: 'postgres',
