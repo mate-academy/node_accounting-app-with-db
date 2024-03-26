9d2b586397e57dec12676e0e0dfcc307d0e14401
unknown <maksoleksyuk@gmail.com>
2024-03-26 19:10:53 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const express = require('express');
+const cors = require('cors');
+
+const { usersRouter } = require('./routes/Users.router');
+const { expensesRouter } = require('./routes/Expenses.router');
+
 const createServer = () => {
-  // your code goes here
+  const app = express();
+
+  app.use(cors());
+  app.use('/users', usersRouter);
+  app.use('/expenses', expensesRouter);
+
+  return app;
 };
 
 module.exports = {
