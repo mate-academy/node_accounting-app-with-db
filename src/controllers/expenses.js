24824d3d860537a9634ecb97bd42a3e022cfb170
unknown <roman.kovalchuk220702@gmail.com>
2024-04-11 10:26:02 +0300

    Solution

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -1,15 +1,15 @@
 import * as expensesServices from '../services/expenses.js';
 
-export const getAll = (req, res) => {
-  const expenses = expensesServices.getAll();
+export const getAll = async (req, res) => {
+  const expenses = await expensesServices.getAll();
 
   res.statusCode = 201;
   res.send(expenses);
 };
 
-export const getById = (req, res) => {
+export const getById = async (req, res) => {
   const { expenseId } = req.params;
-  const expense = expensesServices.getById(expenseId);
+  const expense = await expensesServices.getById(expenseId);
 
   if (!expense) {
     res.sendStatus(404);
@@ -21,29 +21,29 @@ export const getById = (req, res) => {
   res.send(expense);
 };
 
-export const create = (req, res) => {
-  const newExpenses = expensesServices.create(req.body);
+export const create = async (req, res) => {
+  const newExpenses = await expensesServices.create(req.body);
 
   res.statusCode = 201;
   res.send(newExpenses);
 };
 
-export const remove = (req, res) => {
+export const remove = async(req, res) => {
   const { expenseId } = req.params;
-  const foundExpense = expensesServices.getById(expenseId);
+  const foundExpense = await expensesServices.getById(expenseId);
 
   if (!foundExpense) {
     res.sendStatus(404);
 
     return;
   }
-  expensesServices.remove(expenseId);
+  await expensesServices.remove(expenseId);
   res.sendStatus(204);
 };
 
-export const update = (req, res) => {
+export const update = async (req, res) => {
   const { expenseId } = req.params;
-  const foundExpense = expensesServices.getById(expenseId);
+  const foundExpense = await expensesServices.getById(expenseId);
 
   if (!foundExpense) {
     res.sendStatus(404);
@@ -53,7 +53,7 @@ export const update = (req, res) => {
 
   const { name, title, amount, date, category, note } = req.body;
 
-  expensesServices.update({
+  await expensesServices.update({
     id: foundExpense.id, name, title, amount, date, category, note,
   });
   res.send(foundExpense);
