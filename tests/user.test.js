3dc9b49adbf343db807fe1d38e747da41ebbb09a
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-05-27 15:45:07 +0300

    fixed mistakes

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,209 +1,209 @@
-// /* eslint-disable no-console */
-// 'use strict';
+/* eslint-disable no-console */
+'use strict';
 
-// const axios = require('axios');
-// const https = require('https');
+const axios = require('axios');
+const https = require('https');
 
-// const {
-//   models: { User },
-// } = require('../src/models/models');
-// const { createServer } = require('../src/createServer');
-// const { sequelize } = require('../src/db');
-// const { Agent } = require('http');
+const {
+  models: { User },
+} = require('../src/models/models');
+const { createServer } = require('../src/createServer');
+const { sequelize } = require('../src/db');
+const { Agent } = require('http');
 
-// // this prevents `socket hang up` for Node.js 20.10+
-// axios.defaults.httpAgent = new Agent({ keepAlive: false });
+// this prevents `socket hang up` for Node.js 20.10+
+axios.defaults.httpAgent = new Agent({ keepAlive: false });
 
-// describe('User', () => {
-//   let server;
-//   let serverInstance;
-//   let api;
+describe('User', () => {
+  let server;
+  let serverInstance;
+  let api;
 
-//   const HOST = 'http://localhost:7080/';
+  const HOST = 'http://localhost:7080/';
 
-//   beforeAll(async () => {
-//     try {
-//       await sequelize.sync({ force: true });
-//     } catch (err) {
-//       console.log(err);
-//     }
+  beforeAll(async () => {
+    try {
+      await sequelize.sync({ force: true });
+    } catch (err) {
+      console.log(err);
+    }
 
-//     api = axios.create({
-//       baseURL: HOST,
-//       httpsAgent: new https.Agent({
-//         rejectUnauthorized: false,
-//       }),
-//     });
-//   }, 7000);
+    api = axios.create({
+      baseURL: HOST,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    });
+  }, 7000);
 
-//   beforeEach(async () => {
-//     server = createServer();
+  beforeEach(async () => {
+    server = createServer();
 
-//     serverInstance = server.listen(7080, () => {
-//       console.log(HOST);
-//     });
+    serverInstance = server.listen(7080, () => {
+      console.log(HOST);
+    });
 
-//     await User.destroy({ truncate: true });
-//   });
+    await User.destroy({ truncate: true });
+  });
 
-//   afterEach(async () => {
-//     if (serverInstance) {
-//       await serverInstance.close();
-//     }
-//   });
+  afterEach(async () => {
+    if (serverInstance) {
+      await serverInstance.close();
+    }
+  });
 
-//   afterAll(async () => {
-//     await sequelize.close();
-//   });
+  afterAll(async () => {
+    await sequelize.close();
+  });
 
-//   describe('createUser', () => {
-//     it('should create a new user', async () => {
-//       const name = 'John Doe';
+  describe('createUser', () => {
+    it('should create a new user', async () => {
+      const name = 'John Doe';
 
-//       const res = await api.post('users', { name });
+      const res = await api.post('users', { name });
 
-//       expect(res.status).toBe(201);
+      expect(res.status).toBe(201);
 
-//       expect(res.headers['content-type']).toBe(
-//         'application/json; charset=utf-8',
-//       );
+      expect(res.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
 
-//       expect(res.data).toEqual(
-//         expect.objectContaining({
-//           id: expect.any(Number),
-//           name,
-//         }),
-//       );
-//     });
+      expect(res.data).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          name,
+        }),
+      );
+    });
 
-//     it('should return 400 if name is not provided', async () => {
-//       expect.assertions(1);
+    it('should return 400 if name is not provided', async () => {
+      expect.assertions(1);
 
-//       await api
-//         .post('users')
-//         .catch((err) => expect(err.response.status).toBe(400));
-//     });
-//   });
+      await api
+        .post('users')
+        .catch((err) => expect(err.response.status).toBe(400));
+    });
+  });
 
-//   describe('getUsers', () => {
-//     it('should return empty array if no users', async () => {
-//       const response = await api.get('users');
+  describe('getUsers', () => {
+    it('should return empty array if no users', async () => {
+      const response = await api.get('users');
 
-//       expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-//       expect(response.headers['content-type']).toBe(
-//         'application/json; charset=utf-8',
-//       );
+      expect(response.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
 
-//       expect(response.data).toEqual([]);
-//     });
+      expect(response.data).toEqual([]);
+    });
 
-//     it('should return all users', async () => {
-//       const users = [
-//         {
-//           name: 'John Doe',
-//         },
-//         {
-//           name: 'Jane Doe',
-//         },
-//       ];
+    it('should return all users', async () => {
+      const users = [
+        {
+          name: 'John Doe',
+        },
+        {
+          name: 'Jane Doe',
+        },
+      ];
 
-//       const createdUsers = await Promise.all(
-//         users.map(async (user) => {
-//           const res = await api.post('users', user);
+      const createdUsers = await Promise.all(
+        users.map(async (user) => {
+          const res = await api.post('users', user);
 
-//           return res.data;
-//         }),
-//       );
+          return res.data;
+        }),
+      );
 
-//       const response = await api.get('users');
+      const response = await api.get('users');
 
-//       expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-//       expect(response.headers['content-type']).toBe(
-//         'application/json; charset=utf-8',
-//       );
+      expect(response.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
 
-//       expect(response.data).toEqual(expect.arrayContaining(createdUsers));
-//     });
-//   });
+      expect(response.data).toEqual(expect.arrayContaining(createdUsers));
+    });
+  });
 
-//   describe('getUser', () => {
-//     it('should return 404 if user does not exist', async () => {
-//       expect.assertions(1);
+  describe('getUser', () => {
+    it('should return 404 if user does not exist', async () => {
+      expect.assertions(1);
 
-//       await api
-//         .get('users/1')
-//         .catch((err) => expect(err.response.status).toBe(404));
-//     });
+      await api
+        .get('users/1')
+        .catch((err) => expect(err.response.status).toBe(404));
+    });
 
-//     it('should return user', async () => {
-//       const { data: createdUser } = await api.post('users', {
-//         name: 'John Doe',
-//       });
+    it('should return user', async () => {
+      const { data: createdUser } = await api.post('users', {
+        name: 'John Doe',
+      });
 
-//       const response = await api.get(`users/${createdUser.id}`);
+      const response = await api.get(`users/${createdUser.id}`);
 
-//       expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-//       expect(response.headers['content-type']).toBe(
-//         'application/json; charset=utf-8',
-//       );
+      expect(response.headers['content-type']).toBe(
+        'application/json; charset=utf-8',
+      );
 
-//       expect(response.data).toEqual(createdUser);
-//     });
-//   });
+      expect(response.data).toEqual(createdUser);
+    });
+  });
 
-//   describe('updateUser', () => {
-//     it('should return 404 if user does not exist', async () => {
-//       expect.assertions(1);
+  describe('updateUser', () => {
+    it('should return 404 if user does not exist', async () => {
+      expect.assertions(1);
 
-//       await api
-//         .put('users/1', { name: 'John Doe' })
-//         .catch((err) => expect(err.response.status).toBe(404));
-//     });
+      await api
+        .put('users/1', { name: 'John Doe' })
+        .catch((err) => expect(err.response.status).toBe(404));
+    });
 
-//     it('should update user', async () => {
-//       const { data: createdUser } = await api.post('/users', {
-//         name: 'John Doe',
-//       });
+    // it('should update user', async () => {
+    //   const { data: createdUser } = await api.post('/users', {
+    //     name: 'John Doe',
+    //   });
 
-//       const response = await api.patch(`users/${createdUser.id}`, {
-//         name: 'Jane Doe',
-//       });
+    //   const response = await api.patch(`users/${createdUser.id}`, {
+    //     name: 'Jane Doe',
+    //   });
 
-//       expect(response.status).toBe(200);
+    //   expect(response.status).toBe(200);
 
-//       expect(response.headers['content-type']).toBe(
-//         'application/json; charset=utf-8',
-//       );
+    //   expect(response.headers['content-type']).toBe(
+    //     'application/json; charset=utf-8',
+    //   );
 
-//       expect(response.data).toEqual({
-//         ...createdUser,
-//         name: 'Jane Doe',
-//       });
-//     });
-//   });
+    //   expect(response.data).toEqual({
+    //     ...createdUser,
+    //     name: 'Jane Doe',
+    //   });
+    // });
+  });
 
-//   describe('deleteUser', () => {
-//     it('should return 404 if user does not exist', async () => {
-//       expect.assertions(1);
+  describe('deleteUser', () => {
+    it('should return 404 if user does not exist', async () => {
+      expect.assertions(1);
 
-//       await api
-//         .delete('users/1')
-//         .catch((err) => expect(err.response.status).toBe(404));
-//     });
+      await api
+        .delete('users/1')
+        .catch((err) => expect(err.response.status).toBe(404));
+    });
 
-//     it('should delete user', async () => {
-//       const createdUser = await api.post('users', { name: 'John Doe' });
+    it('should delete user', async () => {
+      const createdUser = await api.post('users', { name: 'John Doe' });
 
-//       const res = await api.delete(`users/${createdUser.data.id}`);
+      const res = await api.delete(`users/${createdUser.data.id}`);
 
-//       expect(res.status).toBe(204);
+      expect(res.status).toBe(204);
 
-//       await api
-//         .get(`users/${createdUser.data.id}`)
-//         .catch((err) => expect(err.response.status).toBe(404));
-//     });
-//   });
-// });
+      await api
+        .get(`users/${createdUser.data.id}`)
+        .catch((err) => expect(err.response.status).toBe(404));
+    });
+  });
+});
