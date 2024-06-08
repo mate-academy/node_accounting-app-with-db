c59e8cb2cb0d00c3e13e55e8c9a4a58c0305e9ec
Yevhen Protasov <protasovyevhen@gmail.com>
2024-06-08 16:45:03 +0300

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const express = require('express');
+const cors = require('cors');
+const { router: userRouter } = require('./routes/user.route');
+const { router: expenseRouter } = require('./routes/expense.route');
+
 const createServer = () => {
-  // your code goes here
+  const app = express();
+
+  app.use(cors());
+
+  app.use('/users', express.json(), userRouter);
+  app.use('/expenses', express.json(), expenseRouter);
+
+  return app;
 };
 
 module.exports = {
