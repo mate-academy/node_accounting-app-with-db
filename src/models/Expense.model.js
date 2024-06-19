fcd925d9376e3a6f911896899a427366ca5c323b
Maryna Karpova <mari.artarea@gmail.com>
2024-06-19 14:57:21 +0200

    models

--- src/models/Expense.model.js
+++ src/models/Expense.model.js
@@ -1,11 +1,42 @@
 'use strict';
 
+const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db.js');
 
-const Expense = sequelize
-  .define
-  // your code goes here
-  ();
+const Expense = sequelize.define(
+  'Expense',
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    spentAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    note: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'expenses',
+    createdAt: false,
+    updatedAt: false,
+  },
+);
 
 module.exports = {
   Expense,
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,11 +1,29 @@
+/* eslint-disable no-console */
+/* eslint-disable prettier/prettier */
 'use strict';
 
+const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db.js');
 
-const User = sequelize
-  .define
-  // your code goes here
-  ();
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: 'users',
+    updatedAt: false,
+    createdAt: false,
+  },
+);
 
 module.exports = {
   User,
