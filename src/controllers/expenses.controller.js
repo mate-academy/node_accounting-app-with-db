9403cb4838813ab532c2b9ac904355f2246fb1d9
Yana Momot <140169867+yanamomot@users.noreply.github.com>
2024-07-10 16:00:42 +0300

    Update expenses.controller.js

--- src/controllers/expenses.controller.js
+++ src/controllers/expenses.controller.js
@@ -1,14 +1,20 @@
 const expensesService = require('../services/expenses.service');
 const usersService = require('../services/users.service');
 
+const HTTP_OK = 200;
+const HTTP_BAD_REQUEST = 400;
+const HTTP_NOT_FOUND = 404;
+const HTTP_NO_CONTENT = 204;
+const HTTP_CREATED = 201;
+
 const get = async (req, res) => {
   try {
     const { userId, categories, from, to } = req.query;
     const all = await expensesService.getAll(+userId, categories, from, to);
 
-    return res.status(200).send(all);
+    return res.status(HTTP_OK).send(all);
   } catch (err) {
-    return res.status(400).send(err.message);
+    return res.status(HTTP_BAD_REQUEST).send(err.message);
   }
 };
 
@@ -17,18 +23,18 @@ const getOne = async (req, res) => {
     const { id } = req.params;
 
     if (!id) {
-      return res.status(400).send();
+      return res.status(HTTP_BAD_REQUEST).send();
     }
 
     const expense = await expensesService.getOneExpense(+id);
 
     if (!expense) {
-      return res.status(404).send();
+      return res.status(HTTP_NOT_FOUND).send();
     } else {
-      return res.status(200).send(expense);
+      return res.status(HTTP_OK).send(expense);
     }
   } catch (err) {
-    return res.status(400).send(err.message);
+    return res.status(HTTP_BAD_REQUEST).send(err.message);
   }
 };
 
@@ -40,12 +46,12 @@ const deleting = async (req, res) => {
     if (expense) {
       await expensesService.deletingExpense(+id);
 
-      return res.status(204).send();
+      return res.status(HTTP_NO_CONTENT).send();
     } else {
-      return res.status(404).send();
+      return res.status(HTTP_NOT_FOUND).send();
     }
   } catch (err) {
-    return res.status(400).send(err.message);
+    return res.status(HTTP_BAD_REQUEST).send(err.message);
   }
 };
 
@@ -55,18 +61,18 @@ const patch = async (req, res) => {
     const expData = req.body;
 
     if (!id || !expData) {
-      return res.status(400).send();
+      return res.status(HTTP_BAD_REQUEST).send();
     }
 
     const updated = await expensesService.updateExpense(+id, expData);
 
     if (updated) {
-      return res.status(200).json(updated);
+      return res.status(HTTP_OK).json(updated);
     } else {
-      return res.status(404).send();
+      return res.status(HTTP_NOT_FOUND).send();
     }
   } catch (err) {
-    return res.status(400).send(err.message);
+    return res.status(HTTP_BAD_REQUEST).send(err.message);
   }
 };
 
@@ -76,14 +82,14 @@ const post = async (req, res) => {
     const user = await usersService.getUser(expData.userId);
 
     if (!user) {
-      return res.status(400).send();
+      return res.status(HTTP_BAD_REQUEST).send();
     }
 
     const created = await expensesService.createExpense(expData);
 
-    return res.status(201).send(created);
+    return res.status(HTTP_CREATED).send(created);
   } catch (err) {
-    return res.status(400).send(err.message);
+    return res.status(HTTP_BAD_REQUEST).send(err.message);
   }
 };
 
