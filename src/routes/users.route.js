08af0049e0401458741709de378c90441f97c1b2
Yana Momot <140169867+yanamomot@users.noreply.github.com>
2024-07-10 16:10:59 +0300

    Update users.route.js

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const controller = require('../controllers/users.controller');
 
+const GET_ALL_USERS = '/';
+const GET_ONE_USER = '/:id';
+const CREATE_USER = '/';
+const UPDATE_USER = '/:id';
+const DELETE_USER = '/:id';
+
 const usersRoute = express.Router();
 
-usersRoute.get('/', controller.get);
-usersRoute.get('/:id', controller.getOne);
-usersRoute.post('/', controller.post);
-usersRoute.patch('/:id', controller.patch);
-usersRoute.delete('/:id', controller.deleting);
+usersRoute.get(GET_ALL_USERS, controller.get);
+usersRoute.get(GET_ONE_USER, controller.getOne);
+usersRoute.post(CREATE_USER, controller.post);
+usersRoute.patch(UPDATE_USER, controller.patch);
+usersRoute.delete(DELETE_USER, controller.deleting);
 
 module.exports = {
   usersRoute,
