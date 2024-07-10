1c3b65a2c69b365416c95acbb3c3e7e8d0f1bebe
Yana Momot <140169867+yanamomot@users.noreply.github.com>
2024-07-10 16:13:33 +0300

    Update expenses.route.js

--- src/routes/expenses.route.js
+++ src/routes/expenses.route.js
@@ -2,11 +2,17 @@ const express = require('express');
 const expensesRoute = express.Router();
 const controller = require('../controllers/expenses.controller');
 
-expensesRoute.get('/', controller.get);
-expensesRoute.get('/:id', controller.getOne);
-expensesRoute.post('/', controller.post);
-expensesRoute.patch('/:id', controller.patch);
-expensesRoute.delete('/:id', controller.deleting);
+const GET_ALL_EXPENSES = '/';
+const GET_ONE_EXPENSE = '/:id';
+const CREATE_EXPENSE = '/';
+const UPDATE_EXPENSE = '/:id';
+const DELETE_EXPENSE = '/:id';
+
+expensesRoute.get(GET_ALL_EXPENSES, controller.get);
+expensesRoute.get(GET_ONE_EXPENSE, controller.getOne);
+expensesRoute.post(CREATE_EXPENSE, controller.post);
+expensesRoute.patch(UPDATE_EXPENSE, controller.patch);
+expensesRoute.delete(DELETE_EXPENSE, controller.deleting);
 
 module.exports = {
   expensesRoute,
