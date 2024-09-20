d1229b8d5814c724ecdcc0ed4b79ac9d728b2552
SrTrace <levashov04sergey@gmail.com>
2024-09-20 17:01:39 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const createServer = () => {
-  // your code goes here
-};
+const express = require('express');
+const { userRouter } = require('./routes/usersRoute');
+const { expenseRouter } = require('./routes/expenseRoute');
+
+function createServer() {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use('/users', userRouter);
+  app.use('/expenses', expenseRouter);
+
+  return app;
+}
 
 module.exports = {
   createServer,
