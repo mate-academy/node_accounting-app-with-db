80ad4ed02769f35edf889428955c78769190503f
Vladyslav Shylin <indiggoo228@gmail.com>
2024-10-31 15:08:35 +0200

    add task solution

---
+++ src/controllers/expense.controller.js
@@ -0,0 +1,102 @@
+const expenseService = require('../services/expense.service');
+const userService = require('./../services/user.service');
+
+const getExpenses = async (req, res) => {
+  const { userId, categories, from, to } = req.query;
+
+  try {
+    const expenses = await expenseService.getAllExpense({
+      userId,
+      categories,
+      from,
+      to,
+    });
+
+    res.status(200).send(expenses);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+};
+
+const getOneExpense = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const expense = await expenseService.getExpenseById(id);
+
+    if (!expense) {
+      return res.status(404).send('Not found');
+    }
+    res.send(expense);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+};
+
+const createExpense = async (req, res) => {
+  const data = req.body;
+
+  try {
+    const user = await userService.getUserById(data.userId);
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expense = await expenseService.createExpense(data);
+
+    res.status(201).send(expense);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+};
+
+const updateExpense = async (req, res) => {
+  const { id } = req.params;
+  const { title, amount, category, spentAt, note } = req.body;
+
+  try {
+    const expense = await expenseService.getExpenseById(id);
+
+    if (!expense) {
+      return res.status(404).send('Error');
+    }
+
+    const updatedExpense = await expenseService.updateExpense(id, {
+      title: title !== undefined ? title : expense.title,
+      amount: amount !== undefined ? amount : expense.amount,
+      category: category !== undefined ? category : expense.category,
+      spentAt: spentAt !== undefined ? spentAt : expense.spentAt,
+      note: note !== undefined ? note : expense.note,
+    });
+
+    res.status(200).send(updatedExpense);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+};
+
+const removeExpense = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const exp = await expenseService.getExpenseById(id);
+
+    if (!exp) {
+      return res.status(404).send('Error');
+    }
+
+    await expenseService.removeExpense(id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+};
+
+module.exports = {
+  getExpenses,
+  getOneExpense,
+  createExpense,
+  updateExpense,
+  removeExpense,
+};
---
+++ src/services/expense.service.js
@@ -0,0 +1,70 @@
+const { Op } = require('sequelize');
+const { Expense } = require('../models/Expense.model.js');
+
+const getAllExpense = async ({ userId, categories, from, to }) => {
+  const params = {};
+
+  if (userId) {
+    params.userId = userId;
+  }
+
+  if (categories) {
+    params.category = categories;
+  }
+
+  if (from || to) {
+    params.spentAt = {};
+
+    if (from) {
+      params.spentAt[Op.gte] = from;
+    }
+
+    if (to) {
+      params.spentAt[Op.lte] = to;
+    }
+  }
+
+  const expList = Expense.findAll({ where: params });
+
+  return expList;
+};
+
+const getExpenseById = async (id) => {
+  return Expense.findByPk(id);
+};
+
+const createExpense = async (data) => {
+  return Expense.create(data);
+};
+
+const updateExpense = async (
+  id,
+  { spentAt, title, amount, category, note },
+) => {
+  await Expense.update(
+    {
+      spentAt,
+      title,
+      amount,
+      category,
+      note,
+    },
+    { where: { id } },
+  );
+
+  return Expense.findByPk(id);
+};
+
+const removeExpense = async (id) => {
+  return Expense.destroy({
+    where: { id },
+  });
+};
+
+module.exports = {
+  getAllExpense,
+  getExpenseById,
+  createExpense,
+  updateExpense,
+  removeExpense,
+};
