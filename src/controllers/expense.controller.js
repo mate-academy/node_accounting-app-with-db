46f856a3e03d4d9d6d3034eb1d15ce713c270f80
dmitruz <dmitruz970@gmail.com>
2024-11-06 20:52:58 +0200

    task done

---
+++ src/controllers/expense.controller.js
@@ -0,0 +1,68 @@
+const expenseService = require('../services/expense.service');
+const { getById: getUserById } = require('../services/user.service');
+
+const get = async (req, res) => {
+  const expenses = await expenseService
+    .getAll(req.query)
+    .then((exps) => exps.map(expenseService.normalize));
+
+  res.send(expenses);
+};
+
+const getOne = (req, res) => {
+  const expense = expenseService.normalize(req.entry);
+
+  res.send(expense);
+};
+
+const create = async (req, res) => {
+  const {
+    userId,
+    spentAt = new Date(),
+    title,
+    amount,
+    category,
+    note,
+  } = req.body;
+
+  const user = await getUserById(userId);
+
+  if (!user) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const expense = await expenseService
+    .create(userId, spentAt, title, amount, category, note)
+    .then(expenseService.normalize);
+
+  res.status(201).send(expense);
+};
+
+const remove = async (req, res) => {
+  const { id } = req.entry;
+
+  await expenseService.remove(id);
+  res.sendStatus(204);
+};
+
+const update = async (req, res) => {
+  const { id } = req.entry;
+
+  await expenseService.update(id, req.body);
+
+  const expense = await expenseService
+    .getById(id)
+    .then(expenseService.normalize);
+
+  res.send(expense);
+};
+
+module.exports = {
+  getOne,
+  get,
+  create,
+  remove,
+  update,
+};
--- src/createServer.js
+++ src/createServer.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { router: userRouter } = require('./routes/user.route');
+const { router: expenseRouter } = require('./routes/expense.route');
+
 const createServer = () => {
-  // your code goes here
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.static('public'));
+  app.use('/users', userRouter);
+  app.use('/expenses', expenseRouter);
+
+  return app;
 };
 
 module.exports = {
