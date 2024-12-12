fadf3fbfa299653751898143ee5adb054d029d6b
ab3MN <ab3mn3@gmail.com>
2024-12-12 14:47:51 +0200

    feat:implemented solution

---
+++ src/services/expenses.services.js
@@ -0,0 +1,47 @@
+const { getExpensesFilterQuery } = require('../utils/expenses');
+
+const { Expense } = require('../models/Expense.model');
+
+const getExpenses = async (categories, userId, from, to) => {
+  const where = getExpensesFilterQuery(categories, userId, from, to);
+
+  const expenses = await Expense.findAll({ where });
+
+  return expenses;
+};
+
+const getExpenseById = async (id) => {
+  const expense = await Expense.findByPk(id);
+
+  return expense;
+};
+
+const createExpense = async (expense) => {
+  const newExpense = await Expense.create(expense);
+
+  return newExpense;
+};
+
+const removeExpense = async (id) => {
+  await Expense.destroy({
+    where: {
+      id,
+    },
+  });
+};
+
+const updateExpense = async (id, valuesToUpdate) => {
+  await Expense.update(valuesToUpdate, { where: { id } });
+
+  const updatedExpense = await getExpenseById(id);
+
+  return updatedExpense;
+};
+
+module.exports = {
+  getExpenses,
+  getExpenseById,
+  createExpense,
+  removeExpense,
+  updateExpense,
+};
