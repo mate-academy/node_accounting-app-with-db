b3ccf0aa93d6ab24b8a28074c59880477ec82ac8
Nazar Pastuschak <kns19pna@gmail.com>
2024-12-22 20:08:41 +0200

    add db with express

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,21 @@
 'use strict';
 
-const createServer = () => {
-  // your code goes here
-};
+const express = require('express');
+const cors = require('cors');
+const { router: userRouter } = require('./routes/user.route.js');
+const { router: expenseRouter } = require('./routes/expense.route.js');
+
+function createServer() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use('/users', userRouter);
+  app.use('/expenses', expenseRouter);
+
+  return app;
+}
 
 module.exports = {
   createServer,
