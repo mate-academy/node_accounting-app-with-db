8d14912abb34043fec7eb5496cd8cb407739030d
Mykhailo Lapchynskyi <mykhailolapchynskyi@b4:4c:3b:52:55:03.local>
2025-01-16 13:25:31 +0100

    add task solution

--- src/setup.js
+++ src/setup.js
@@ -1,14 +1,4 @@
 const { sequelize } = require('./db.js');
-// const { User } = require('./models/User.model');
-// const { Expense } = require('./models/Expense.model');
-
-// User.hasMany(Expense, {
-//   foreignKey: 'userId',
-// });
-//
-// Expense.belongsTo(User, {
-//   foreignKey: 'userId',
-// });
 
 async function setupDatabase() {
   try {
