831e28edaa6e903e60e6bb4244f57a4eb16251b6
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2025-01-23 14:55:54 +0200

    implemented models for expenses and users

--- src/models/Expense.model.js
+++ src/models/Expense.model.js
@@ -1,9 +1,49 @@
 'use strict';
 
 const { sequelize } = require('../db.js');
+const { DataTypes } = require('sequelize');
 
 const Expense = sequelize.define(
-  // your code goes here
+  'Expense',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+      allowNull: false,
+    },
+    spentAt: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      // defaultValue:
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    note: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: '',
+    },
+  },
+  {
+    tableName: 'expenses',
+    updatedAt: false,
+    createdAt: false,
+  },
 );
 
 module.exports = {
