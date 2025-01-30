901d69f92e24d08542f9417d64105f5810395bf8
marichkamt <emilycry1999@gmail.com>
2025-01-30 13:42:53 +0100

    add task solution

---
+++ src/routes/expenseRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const expenseController = require('../controllers/expenseController');
+
+const expenseRouter = express.Router();
+
+expenseRouter.get('/', expenseController.get);
+expenseRouter.get('/:id', expenseController.getOne);
+expenseRouter.post('/', expenseController.create);
+expenseRouter.delete('/:id', expenseController.remove);
+expenseRouter.patch('/:id', expenseController.update);
+
+module.exports = { expenseRouter };
---
+++ src/services/expensesService.js
@@ -0,0 +1,65 @@
+const { Op } = require('sequelize');
+const { models } = require('../models/models');
+const { Expense } = models;
+
+const getAllExpenses = async (userId, categories, from, to) => {
+  const whereClause = {};
+
+  if (userId) {
+    whereClause.userId = userId;
+  }
+
+  if (categories && categories.length > 0) {
+    whereClause.category = categories;
+  }
+
+  if (from && to) {
+    whereClause.spentAt = { [Op.between]: [from, to] };
+  } else if (from) {
+    whereClause.spentAt = { [Op.gte]: from };
+  } else if (to) {
+    whereClause.spentAt = { [Op.lte]: to };
+  }
+
+  return Expense.findAll({ where: whereClause });
+};
+
+const getExpenseById = async (id) => {
+  return Expense.findByPk(id);
+};
+
+const createExpense = async (
+  userId,
+  spentAt,
+  title,
+  amount,
+  category,
+  note,
+) => {
+  return Expense.create({
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  });
+};
+
+const updateExpense = async (id, data) => {
+  await Expense.update({ ...data }, { where: { id } });
+
+  return Expense.findByPk(id);
+};
+
+const deleteExpense = async (id) => {
+  return Expense.destroy({ where: { id } });
+};
+
+module.exports = {
+  getAllExpenses,
+  getExpenseById,
+  createExpense,
+  updateExpense,
+  deleteExpense,
+};
